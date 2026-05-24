333e0fd093ac691522b78995ecc7620652b1b6e9
MaulanaN <maulanaqaswarah@gmail.com>
2026-05-24 14:08:30 +0700

    Update streamlit_chat_app.py
    added slider for system config

--- streamlit_chat_app.py
+++ streamlit_chat_app.py
@@ -66,9 +66,14 @@ if "chat" not in st.session_state:
     # Session ini menyimpan konteks percakapan di sisi Gemini
     st.session_state.chat = st.session_state.genai_client.chats.create(
         model="gemini-2.5-flash",
-        config={"system_instruction": "Kamu adalah asisten yang hanya menjawab kebutuhan tentang manajerial konten platform wiki."}
+        config={"system_instruction": "Kamu adalah asisten yang hanya menjawab kebutuhan tentang manajerial konten platform wiki. Sebagai asisten platform wiki, Anda sangat penting untuk menguasai Module dan Template pada MediaWiki."},
+        temperature=temp,
+        top_p=TopP,
+        top_k=TopK,
     )
-    st.sidebar.slider("Temperature", 0.0, 1.0, 0.7)
+    temp = st.sidebar.slider("Temperature", 0.0, 1.0, 0.7)
+    TopP = st.sidebar.slider("Top P", 0.0, 1.0, 0.95)
+    TopK = st.sidebar.slider("Top K", 0, 50, 40)
 
 # Inisialisasi list riwayat pesan kalau belum ada
 # List ini menyimpan semua pesan untuk ditampilkan kembali saat skrip dijalankan ulang
